94612dd74763b897f627bc08e713e2cf95ee74fa
Chinweike99 <Innocentakwolu1@gmail.com>
2025-05-12 08:37:00 +0100

    fix: login error

--- backend_toolbox/micro_services/identity-service/src/routes/identity_service.js
+++ backend_toolbox/micro_services/identity-service/src/routes/identity_service.js
@@ -4,6 +4,7 @@ import * as userAuth from '../controllers/indentity_controller.js';
 const router = express.Router();
 
 router.post('/register', userAuth.registerUser);
+router.post('/login', userAuth.loginUser)
 
 
 export default router;
--- backend_toolbox/micro_services/identity-service/src/utils/validation.js
+++ backend_toolbox/micro_services/identity-service/src/utils/validation.js
@@ -11,9 +11,32 @@ export const validateReg = (data) => {
 }
 
 
+// export const validateLogin = (data) => {
+//     const schema = Joi.object({
+//         email: Joi.string().email({tlds: {allow: false}}).required().message({"string.email": "Enter a valid email addrress"}),
+//         password: Joi.string().min(6).required().messages({'string.min': "Password should be more than six characters"})
+//     });
+//     const {error, value} = schema.validate(data);
+//     if(error){
+//         throw error
+//     };
+//     return value;
+// }
+
+
 export const validateLogin = (data) => {
     const schema = Joi.object({
-        email: Joi.string().email("Enter a valid email addrress"),
-        password: Joi.string().min(6, "Password should be more than six characters")
-    })
-}
+        email: Joi.string().email().required().messages({
+            "string.email": "Enter a valid email address",
+            "string.empty": "Email cannot be empty",
+            "any.required": "Email is required"
+        }),
+        password: Joi.string().min(6).required().messages({
+            "string.min": "Password should be more than six characters",
+            "string.empty": "Password cannot be empty",
+            "any.required": "Password is required"
+        }),
+    });
+
+    return schema.validate(data, { abortEarly: false }); // validate all fields
+};
