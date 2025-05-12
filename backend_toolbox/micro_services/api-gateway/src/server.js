9330c16d97d7917d63abf116508093ac76d29c16
Chinweike99 <Innocentakwolu1@gmail.com>
2025-05-12 09:01:20 +0100

    updated

--- backend_toolbox/micro_services/api-gateway/src/server.js
+++ backend_toolbox/micro_services/api-gateway/src/server.js
@@ -1,1 +1,12 @@
-console.log("Hi")
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import Redis from 'ioredis';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const redisClient = new Redis(process.env.REDIS_URL)
