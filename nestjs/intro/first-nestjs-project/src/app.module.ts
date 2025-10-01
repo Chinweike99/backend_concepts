6150cdde3f181787cfd45b8be634313d0de33574
Chinweike99 <innocentakwolu1@gmail.com>
2025-10-01 17:28:05 +0100

    nestJs

--- nestjs/intro/first-nestjs-project/src/app.module.ts
+++ nestjs/intro/first-nestjs-project/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TestController } from './test/test.controller';
+import { Test2Controller } from './test2/test2.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UsersModule],
+  controllers: [AppController, TestController, Test2Controller],
   providers: [AppService],
 })
 export class AppModule {}
