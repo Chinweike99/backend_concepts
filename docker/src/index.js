0a36744b52bceba85dd80e329bca01115fdd4227
Chinweike99 <innocentakwolu1@gmail.com>
2025-10-04 09:32:33 +0100

    docker intro

---
+++ docker/src/index.js
@@ -0,0 +1,15 @@
+
+import express from "express";
+import dotenv from "dotenv";
+import routes from "./routes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use("/todos", routes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
